08dc2c2f3c6662ca26ac102e2e282703bfc1653c
Amirkhan Salan <68302923+n7wt@users.noreply.github.com>
2023-01-23 01:21:08 +0300

    java-script practice

---
+++ js-practice/Курс Доллара/js/main.js
@@ -0,0 +1,4 @@
+let CurrentCurrence = prompt("Введите актуальный курс рубля к доллару: ");
+let AmountOfRubles = prompt("Введите доступное количество рублей: ");
+
+alert("Вы можете приобрести: $" + AmountOfRubles / CurrentCurrence);
